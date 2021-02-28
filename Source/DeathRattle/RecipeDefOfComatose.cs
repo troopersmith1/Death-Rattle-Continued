3afec347cd19414a7a3781bfc6bb6b010a359f2c
troopersmith1 <62363949+troopersmith1@users.noreply.github.com>
2021-02-28 11:13:47 -0500

    Add files via upload

---
+++ Source/DeathRattle/RecipeDefOfComatose.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace DeathRattle
+{
+    [DefOf]
+    public static class RecipeDefOfComatose
+    {
+        public static RecipeDef ArtificialComa;
+    }
+}
---
+++ Source/DeathRattle/Recipe_AdministerComaDrug.cs
@@ -0,0 +1,37 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace DeathRattle
+{
+    public class Recipe_AdministerComaDrug : Recipe_Surgery
+    {
+        public override void ApplyOnPawn(Pawn pawn, BodyPartRecord part, Pawn billDoer, List<Thing> ingredients, Bill bill)
+        {
+            if (!pawn.RaceProps.IsFlesh)
+            {
+                return;
+            }
+            pawn.health.forceIncap = true;
+
+            Hediff_Comatose ailment = HediffMaker.MakeHediff(HediffDefOfComatose.ArtificialComa, pawn) as Hediff_Comatose;
+            pawn.health.AddHediff(ailment);
+
+            pawn.health.forceIncap = false;
+        }
+        /*
+        public override void ConsumeIngredient(Thing ingredient, RecipeDef recipe, Map map) {
+        }
+*/
+        public override bool IsViolationOnPawn(Pawn pawn, BodyPartRecord part, Faction billDoerFaction)
+        {
+            if (pawn.Faction == billDoerFaction)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
